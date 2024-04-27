db579c958bb6cf2c0700a9ea4b3b7c0aa525c50b
ghettsebuu <getsemanihidalgo@gmail.com>
2024-04-27 16:22:59 -0400

    niveles agregados - escenarios y personajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // App.js
 import React from 'react';
-//import Dashboard from './components/Dashboard/dashboard'; // Importa tu componente Dashboard
-import HomeScreen from './components/Home/HomeScreen';
+import Dashboard from './components/Dashboard/dashboard'; // Importa tu componente Dashboard
+//import HomeScreen from './components/Home/HomeScreen';
 
 function App() {
   return (
     <div className="App">
-     <HomeScreen/> {/* <Dashboard />  */}
+         <Dashboard /> {/*   <HomeScreen/> */}
     </div>
   );
 }
--- src/components/Home/RexImage.jsx
+++ src/components/Home/RexImage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const RexImage = () => {
   return (
     <img src="/img/dinoazul2.png" alt="DinoRex" className="rex-image dinos dinosaur-animation " />
-  );
+  ); 
 };
 
 export default RexImage;
